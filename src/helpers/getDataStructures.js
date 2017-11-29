4e4916c5b3dbeccca9bb80be8bc6303920f4e0fd
Julien Zarka <julien.zarka@mobile-devices.fr>
2017-11-29 14:03:33 +0100

    Helper: fix data structures tree parsing.

--- src/helpers/getDataStructures.js
+++ src/helpers/getDataStructures.js
@@ -1,11 +1,24 @@
-export default function getDataStructures(result) {
-    while (
-            !result.meta ||
-            !result.meta.classes ||
-            !result.meta.classes.includes('dataStructures')
-    ) {
-        result = result.content[0];
+function getDataStructure(result) {
+  if (
+    !result.meta ||
+      !result.meta.classes ||
+      !result.meta.classes.includes('dataStructures')
+  ) {
+    if (!result.content)
+      return [];
+    var x = 0;
+    for (; x < result.content.length; ++x) {
+      var local_result = getDataStructure(result.content[x]);
+      if (local_result.length > 0)
+        res.push.apply(res, local_result);
     }
+  } else {
+    return result.content;
+  }
+  return res;
+}
 
-    return result.content.map(entry => entry.content[0]);
+export default function getDataStructures(result) {
+  result = getDataStructure(result);
+  return result.map(entry => entry.content[0]);
 }
